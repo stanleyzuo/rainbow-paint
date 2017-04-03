c29c557a35522d1278af72e90b6e14154737cf42
Stanley Zuo <sjrzuo@gmail.com>
2017-04-03 01:05:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,17 @@
-var colors = [[255], [255,0,0], [255,127,0], [255,255,0], [0,255,0], [0,0,255], [75,0,130], [139,0,255]];
-
-var index = 0;
+var colors = [[255,0,0], [255,127,0], [255,255,0], [0,255,0], [0,0,255], [75,0,130], [139,0,255]];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  background(135, 206, 235);
 }
 
 function draw() {
   if (keyIsPressed === true) {
-    clear();
+    background(135,206,235);
     }
-  ellipse(mouseX, mouseY, 50, 50);
-  fill(colors[index]);
-  noStroke();
-}
-
-function mousePressed() {
-  index = index + 1;
-
-  if (index == colors.length) {
-    index = 0;
+  for (var i = 0; i <7; i++) {
+    fill(colors[i]);
+    ellipse(mouseX, 20*i + mouseY, 50,50);
+    noStroke();
   }
 }
